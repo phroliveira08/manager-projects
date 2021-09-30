9eb28fc8a3db206ba400fb2e99f03253ce32fba9
Pedro Oliveira <phr.oliveira08@gmail.com>
2021-09-30 19:36:02 -0300

    print a important message

---
+++ src/main/java/com/mycompany/unodafirma/Main.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.unodafirma;
+
+public class Main {
+    public static void main(String args[]){
+        System.out.print("UNO DA FIRMA 3.0");
+    }
+}
