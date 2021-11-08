70c62f1690a9e777169d791639de5885543f4698
LuisHenriqueOliveira <52217846+LuisHenriqueOliveira@users.noreply.github.com>
2021-11-08 20:04:34 -0300

    Update UDF.sql
    Correção do Script

--- Script_MySQL/UDF.sql
+++ Script_MySQL/UDF.sql
@@ -4,23 +4,23 @@ USE UDF;
 
 CREATE TABLE usuario(
 	username varchar(20) primary key not null,
-    nomeCompleto varchar(255),
-    telefone varchar(15),
-    email varchar(255),
-    cargo varchar(255),
-    senha varchar(20)
+    	nomeCompleto varchar(255),
+    	telefone varchar(15),
+    	email varchar(255),
+    	cargo varchar(255),
+    	senha varchar(20)
 );
 
 CREATE TABLE projeto(
 	idProjeto int auto_increment primary key not null,
-    nomeProjeto varchar(255),
-    descricao varchar(255),
-    usuarioProprietario varchar(20),
+    	nomeProjeto varchar(255),
+    	descricao varchar(255),
+    	usuarioProprietario varchar(20) not null,
 		foreign key (usuarioProprietario) references usuario(username)
 );
 
 CREATE TABLE requisitos(
-	id_requisito int auto_increment primary key not null,
+	identificador int auto_increment primary key not null,
 	nome varchar(255),
 	modulo varchar(255),
 	funcionalidades varchar(255),
@@ -31,20 +31,10 @@ CREATE TABLE requisitos(
 	versao float,
 	prioridade varchar(255),
 	complexidade varchar(255),
-	esfoçoEmHoras int, 
+	esfoçoEmHoras varchar(20), 
 	estado varchar(255), #(Especificado, Em andamento, Finalizado, ...)
 	fase varchar(255),
 	descrição varchar(255),
-    		foreign key (autorUltimaModificacao) references usuario(username),
-            foreign key (autor) references usuario(username)
-);
-
-CREATE TABLE projeto(
-	id_projeto int auto_increment primary key not null,
-    	nomeProjeto varchar(255),
-    	descricao varchar(255),
-    	usuarioProprietario varchar(20),
-    		foreign key (usuarioProprietario) references usuario(username),
-    	fk_id_requisitos int,
-    		foreign key (fk_id_Requisitos) references requisitos(id_requisito)
+    	fk_idProjeto int not null,
+		foreign key (fk_idProjeto) references projeto(idProjeto)
 );
