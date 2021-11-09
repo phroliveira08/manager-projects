e1ce138126bd72c94b4afd22c5ad23741f20f0e6
Kelvin Lima <kelvin_lima94@hotmail.com>
2021-11-08 21:46:29 -0300

    Ajuste Script

--- Script_MySQL/UDF.sql
+++ Script_MySQL/UDF.sql
@@ -20,21 +20,21 @@ CREATE TABLE projeto(
 );
 
 CREATE TABLE requisitos(
-	identificador int auto_increment primary key not null,
+	idRequisito int auto_increment primary key not null,
 	nome varchar(255),
 	modulo varchar(255),
 	funcionalidades varchar(255),
-	dataCriação date,
+	dataCriacao date,
 	autor varchar(255),
 	dataUltimaAlteracao date,
 	autorUltimaModificacao varchar(20),
 	versao float,
 	prioridade varchar(255),
 	complexidade varchar(255),
-	esfoçoEmHoras varchar(20), 
+	esforcoEmHoras int, 
 	estado varchar(255), #(Especificado, Em andamento, Finalizado, ...)
 	fase varchar(255),
-	descrição varchar(255),
+	descricao varchar(255),
     	fk_idProjeto int not null,
 		foreign key (fk_idProjeto) references projeto(idProjeto)
 );
